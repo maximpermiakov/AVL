f65d89247bbdd8c80d2bd9907edab09dc1257472
maximpermiakov <88048869+maximpermiakov@users.noreply.github.com>
2023-04-06 12:32:03 +0300

    Tree struct and creation

--- AVL.cpp
+++ AVL.cpp
@@ -7,3 +7,18 @@ struct Node
     Node *right = nullptr;
     int height = 1;
 };
+
+struct Tree
+{
+    Node *ROOT;
+    Node *NIL;
+};
+
+Tree *create_empty_tree()
+{
+    Tree *tree = new Tree;
+    Node *NIL = new Node;
+    tree->NIL = NIL;
+    tree->ROOT = NIL;
+    return tree;
+}
