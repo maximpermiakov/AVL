c35313feb3d3ff3db58bb4a2c13677cfc66eaeb0
maximpermiakov <88048869+maximpermiakov@users.noreply.github.com>
2023-04-12 19:12:43 +0300

    all

--- AVL.cpp
+++ AVL.cpp
@@ -8,17 +8,134 @@ struct Node
     int height = 1;
 };
 
-struct Tree
+int bfactor(Node *node)
 {
-    Node *ROOT;
-    Node *NIL;
-};
+    return node->right->height - node->left->height;
+}
 
-Tree *create_empty_tree()
+void fixheight(Node *node)
 {
-    Tree *tree = new Tree;
-    Node *NIL = new Node;
-    tree->NIL = NIL;
-    tree->ROOT = NIL;
-    return tree;
+    node->height = std::max(node->left->height, node->right->height) + 1;
+}
+
+Node *rotate_right(Node *p)
+{
+    Node *q = p->left;
+    p->left = q->right;
+    q->right = p;
+    fixheight(p);
+    fixheight(q);
+    return q;
+}
+
+Node *rotate_left(Node *p)
+{
+    Node *q = p->right;
+    p->right = q->left;
+    q->left = p;
+    fixheight(p);
+    fixheight(q);
+    return q;
+}
+
+Node *balance(Node *p)
+{
+    fixheight(p);
+    if (bfactor(p) == 2)
+    {
+        if (bfactor(p->right) < 0)
+        {
+            p->right = rotate_right(p->right);
+        }
+        return rotate_left(p);
+    }
+    if (bfactor(p) == -2)
+    {
+        if (bfactor(p->left) > 0)
+        {
+            p->left = rotate_left(p->left);
+        }
+        return rotate_right(p);
+    }
+    return p;
+}
+
+Node *push(Node *root, int new_key)
+{
+    if (root == nullptr)
+    {
+        root = new Node;
+        root->key = new_key;
+        return root;
+    }
+    if (new_key > root->key)
+    {
+        root->right = push(root->right, new_key);
+    }
+    else
+    {
+        root->left = push(root->left, new_key);
+    }
+    return balance(root);
+}
+
+Node *findmin(Node *p)
+{
+    if (p->left == nullptr)
+        return p;
+    return findmin(p->left);
+}
+
+Node *removemin(Node *p)
+{
+    if (p->left == nullptr)
+        return p->right;
+    p->left = removemin(p->left);
+    return balance(p);
+}
+
+Node *pop(Node *root, int key)
+{
+    if (root == nullptr)
+        return nullptr;
+    if (key > root->key)
+    {
+        root->right = pop(root->right, key);
+    }
+    else if (key < root->key)
+    {
+        root->left = pop(root->left, key);
+    }
+    else
+    {
+        Node *left = root->left;
+        Node *right = root->right;
+        delete root;
+        if (right == nullptr)
+            return left;
+        Node *min = findmin(right);
+        min->right = removemin(right);
+        min->left = left;
+        return balance(min);
+    }
+    return balance(root);
+}
+
+Node *find(Node *root, int key)
+{
+    if (root == nullptr or root->key == key)
+        return root;
+    if (root->key > key)
+        return find(root->left, key);
+    if (root->key < key)
+        return find(root->right, key);
+}
+
+void destroy_tree(Node *root)
+{
+    if (root->left != nullptr)
+        destroy_tree(root->left);
+    if (root->right != nullptr)
+        destroy_tree(root->right);
+    delete root;
 }
